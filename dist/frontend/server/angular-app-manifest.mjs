6249398b5c97e111e7d0cfa1d39be295c4d53b96
Nantenaina Davidson ANDRIAMIHOATRA <FUNFIA-SMIE>
2026-06-09 11:32:31 +0300

    af

--- dist/frontend/server/angular-app-manifest.mjs
+++ dist/frontend/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5636, hash: '1690623f062a442d477f50d5c48d2fb0673e114d72ce25db14543a886b5d32f9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1018, hash: '51059efaae8cd29b8906806ab6635c34abe37da845fbcd32286b617bb81c14b5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 53296, hash: '1b760e0fc03fa6be3f199b650b9be8b4061a1daf7cd07982da38f6c6aae0a340', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5636, hash: '43e237745ba63f82800f3952d3d8689871c344771a94d04f61a6fa157516adeb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1018, hash: '755c8ffe880af7e2eb2f530facdc7f152e3d6b2ce3fc527c71d78d6507f430cf', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 52918, hash: '48a778a9b1f83dc6ee710ba82d0685c7b1aa330c1d459852660c8cb75b8eede3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-DCBSN3S6.css': {size: 22359, hash: 'o2pS6XjtfA4', text: () => import('./assets-chunks/styles-DCBSN3S6_css.mjs').then(m => m.default)}
   },
 };
